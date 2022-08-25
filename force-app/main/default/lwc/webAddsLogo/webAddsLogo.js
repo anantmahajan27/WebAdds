d01660aa4f879cacb785a23879228ead7a7f2aad
anantmahajan27 <anantmahajan27@gmail.com>
2022-08-25 09:57:44 +0530

    co

---
+++ force-app/main/default/lwc/webAddsLogo/webAddsLogo.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import weblogo  from '@salesforce/resourceUrl/webaddslogo';
+
+export default class WebAddsLogo extends LightningElement {
+    sfimage=weblogo ;
+}
